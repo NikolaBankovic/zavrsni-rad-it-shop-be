36959480a745bdf5e0488b14b23de5456da18778
Nikola Bankovic <nikola.bankovic@fer.hr>
2024-05-08 13:47:10 +0200

    Move UserDto to dto package

---
+++ src/main/java/hr/fer/zavrsni1500/itshop/model/OrderItem.java
@@ -0,0 +1,27 @@
+package hr.fer.zavrsni1500.itshop.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "order_item")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private int quantity;
+    private double totalPrice;
+}
