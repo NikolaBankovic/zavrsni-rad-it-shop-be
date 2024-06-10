9c5cbbeddc7ac230c8260c44f26f94ac2592d1dc
Nikola Bankovic <nikola.bankovic@fer.hr>
2024-06-10 16:07:01 +0200

    Update specification for filtering

--- src/main/java/hr/fer/zavrsni1500/itshop/dto/filter/PeripheralFilter.java
+++ src/main/java/hr/fer/zavrsni1500/itshop/dto/filter/PeripheralFilter.java
@@ -3,8 +3,10 @@ package hr.fer.zavrsni1500.itshop.dto.filter;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @RequiredArgsConstructor
 public class PeripheralFilter extends ProductFilter {
