d88fd3377c91f2102abcc8bd108108c4751800b3
Nikola Bankovic <nikola.bankovic@fer.hr>
2024-07-25 01:49:37 +0200

    Add repositories

--- src/main/java/hr/fer/zavrsni1500/itshop/repository/CartRepository.java
+++ src/main/java/hr/fer/zavrsni1500/itshop/repository/CartRepository.java
@@ -2,9 +2,11 @@ package hr.fer.zavrsni1500.itshop.repository;
 
 import hr.fer.zavrsni1500.itshop.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
     Optional<Cart> findByUserId(Long userId);
 }
